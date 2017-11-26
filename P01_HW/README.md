845c30a3b1a731ffcae452c678557717295f1f2b
bruskfil <filip.bruska@tieto.com>
2017-11-26 09:16:23 +0100

    Init for test case + Readme update

--- P01_HW/README.md
+++ P01_HW/README.md
@@ -3,7 +3,12 @@ First Java homework was focused on finding max value from input double array
 
 ## Issues
 1. Problem to run ParameterizedTest in JUnit 5
+	* Temporarily solved by writing many methods with different input
 2. 
+
+## Questions
+1. Should we use @BeforeAll for init?
+2. How to use ParameterizedTest in JUnit 5?
 
 ## Missing functionality
 Find n biggest distinctive (= each number is different) numbers in the given input array. The returned numbers are sorted descending (= from the biggest to the smallest).
--- P01_HW/src/test/java/com/tieto/ecm/training/java/ArrayHandlerTest.java
+++ P01_HW/src/test/java/com/tieto/ecm/training/java/ArrayHandlerTest.java
@@ -3,6 +3,7 @@ package com.tieto.ecm.training.java;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 //@RunWith(Parameterized.class)
@@ -25,71 +26,68 @@ class ArrayHandlerTest {
 	
 	@Test
 	public void testFindMax() {
-		ArrayHandler arrayHandler = new ArrayHandler();
+
 		assertEquals(maxValue, arrayHandler.findMax(inputArray));
 	}
 	*/	
 	
+	static ArrayHandler arrayHandler;
+
+	@BeforeAll
+	static void init() {
+		arrayHandler = new ArrayHandler();
+	}
+
 	@Test
 	void testFindMaxCommonArray() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {23.1d, -56.794611d, -0.0e10, 1.5e2, 42};
+		double[] inputArray = { 23.1d, -56.794611d, -0.0e10, 1.5e2, 42 };
 		assertEquals(150.0, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxOneItem() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {0d};
+		double[] inputArray = { 0d };
 		assertEquals(0, arrayHandler.findMax(inputArray));
 	}
-	
 	@Test
 	void testFindMaxNegativeArray() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {-23.1d, -56.794611d, -0.1e10, -1.5e2, -42};
+		double[] inputArray = { -23.1d, -56.794611d, -0.1e10, -1.5e2, -42 };
 		assertEquals(-23.1, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxFirstItemAsMax() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {0.1, -23.1d, -56.794611d, -0.1e10, -1.5e2, -42, 0.0};
+		double[] inputArray = { 0.1, -23.1d, -56.794611d, -0.1e10, -1.5e2, -42, 0.0 };
 		assertEquals(0.1, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxLastItemAsMax() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {-23.1d, -56.794611d, -0.1e10, -1.5e2, -42, 0.0};
+		double[] inputArray = { -23.1d, -56.794611d, -0.1e10, -1.5e2, -42, 0.0 };
 		assertEquals(0, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxAllValuesSame() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {-23.1d, -23.1d, -23.1d};
+		double[] inputArray = { -23.1d, -23.1d, -23.1d };
 		assertEquals(-23.1d, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxNullArray() {
-		ArrayHandler arrayHandler = new ArrayHandler();
 		double[] inputArray = null;
 		assertEquals(Double.NaN, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxEmptyArray() {
-		ArrayHandler arrayHandler = new ArrayHandler();
 		double[] inputArray = new double[0];
 		assertEquals(Double.NaN, arrayHandler.findMax(inputArray));
 	}
-	
+
 	@Test
 	void testFindMaxFailCheck() {
-		ArrayHandler arrayHandler = new ArrayHandler();
-		double[] inputArray = {23.1d, -56.794611d, -0.0e10, 1.5e2, 42};
+		double[] inputArray = { 23.1d, -56.794611d, -0.0e10, 1.5e2, 42 };
 		assertNotEquals(42.0, arrayHandler.findMax(inputArray));
 	}
 	
