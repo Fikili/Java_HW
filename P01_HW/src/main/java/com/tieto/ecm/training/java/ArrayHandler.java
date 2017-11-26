0a82d465a2aa5e724a6ee69c48f57710026ae30e
bruskfil <filip.bruska@tieto.com>
2017-11-26 09:01:20 +0100

    Adding comments

--- P01_HW/src/main/java/com/tieto/ecm/training/java/ArrayHandler.java
+++ P01_HW/src/main/java/com/tieto/ecm/training/java/ArrayHandler.java
@@ -29,10 +29,13 @@ public class ArrayHandler {
      * Find the biggest number in the given input array using DualPivotQuicksort via java.util.Arrays.sort
      * This algorithm offers O(n log(n)) performance on many data sets
      * 
+     * <strong>>It is not used now </strong> but it could be good for verification results in test cases
+     * 
      * @param input An array containing values. The {@code null} value <strong>is</strong> allowed.
      * @return The biggest number in the array. 
      *         If the array is empty or {@code null}, returns {@code Double.NaN}
      */
+	
     public double findMaxUsingArrays(double[] input) {
     	if (input != null && input.length > 0) {
     		Arrays.sort(input);
--- P01_HW/src/main/java/com/tieto/ecm/training/java/Main.java
+++ P01_HW/src/main/java/com/tieto/ecm/training/java/Main.java
@@ -3,6 +3,7 @@ package com.tieto.ecm.training.java;
 public class Main {
 
 	public static void main(String[] args) {
+		// A few test examples for input data
 		//double[] inputArray = {23.1d, -56.794611d, -0.0e10, 1.5e2, 42};
 		//double[] inputArray = {-23.1d, -56.794611d, -0.1e10, -1.5e2, -42};
 		//double[] inputArray = {-23.1d, -56.794611d, -0.1e10, (Double) null, -42};
