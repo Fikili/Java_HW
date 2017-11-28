d894ccf42938f509ecb2d930e5fd5ae6b0c89631
bruskfil <filip.bruska@tieto.com>
2017-11-28 20:30:34 +0100

    Removed unused import

--- P01_HW/src/test/java/com/tieto/ecm/training/java/ArrayHandlerTest.java
+++ P01_HW/src/test/java/com/tieto/ecm/training/java/ArrayHandlerTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 //@RunWith(Parameterized.class)
